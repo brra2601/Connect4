babe94b5f2dc99169b4e4ea505be7a84973d03f6
br <brandon.rajkowski@gmail.com>
2018-12-13 18:50:46 -0700

    constants are now defined in a class

--- Connect4/Engine.cs
+++ Connect4/Engine.cs
@@ -7,9 +7,6 @@ namespace Connect4
         protected Player player1;
         protected Player player2;
 
-        protected const short rows = 6;
-        protected const short cols = 7;
-
         public Engine()
         {
             player1 = new Player();
@@ -71,11 +68,12 @@ namespace Connect4
 
         protected bool IsValidMove(short columnIndex)
         {
+            const short cols = Connect4Constants.Dimensions.cols;
             if (columnIndex < 0 || columnIndex >= cols)
             {
                 return false;
             }
-            ulong fullColumn = (ulong)0x810204081 << columnIndex;
+            ulong fullColumn = Connect4Constants.Masks.fullColumn << columnIndex;
             ulong player1Positions = player1.GetColumn(columnIndex);
             ulong player2Positions = player2.GetColumn(columnIndex);
             ulong columnAtIndex = player1Positions | player2Positions;
@@ -96,6 +94,7 @@ namespace Connect4
             ulong mask = (ulong)0x1 << columnIndex;
             while((columnAtIndex & mask) == 0x0)
             {
+                const short rows = Connect4Constants.Dimensions.rows;
                 columnAtIndex = columnAtIndex >> 7;
                 rowIndex++;
                 if (rowIndex == rows)
@@ -113,6 +112,5 @@ namespace Connect4
         protected abstract void InformUserInvalidMove();
         protected abstract void InformPlayer1Win();
         protected abstract void InformPlayer2Win();
-
     }
 }
